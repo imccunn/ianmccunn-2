2a7c36b007f20b28c7f165de1ce6fea068822564
Ian McCunn <ianmccunn@gmail.com>
2016-12-04 13:19:53 -0800

    Build process
    - Add babel to compile es6
    - Create basic web server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-// Author: Ian McCunn
+import express from 'express';
+const app = express();
 
-'use strict';
+app.use('/', (req, res) => {
+  res.status(200).send({msg: 'hello'})
+});
 
+app.listen(8080, () => {
+  console.log('Server listening on 8080');
+});
 

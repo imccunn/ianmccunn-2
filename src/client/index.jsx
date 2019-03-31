c16af9807e05cdff308001a2b4a4e7d1fa1c53cb
imccunn <ianmccunn@gmail.com>
2019-03-31 16:05:45 -0700

    Update deps.
    - Reconfigure dev script.

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Nav from './Nav.jsx';
 import Routes from './Routes.jsx';
 
-export default class Index extends Component {
+export default class Index extends React.Component {
     constructor(props) {
       super(props);
     }
 
     render() {
+      console.log('rendering index');
       return (
         <Router>
           <Nav />
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: __dirname + '/src/client',
@@ -15,15 +14,12 @@ module.exports = {
           loader: 'babel-loader',
           exclude: /node_modules/,
           options: {
-            presets: ['es2015', 'react']
+            presets: ['@babel/react']
           }
         }
       ]
     },
     plugins: [
-      new CopyWebpackPlugin([
-        {from: './styles/main.css', to: './styles/main.css'}
-      ])
     ],
     devServer: {
       contentBase: path.join(__dirname, 'public'),
